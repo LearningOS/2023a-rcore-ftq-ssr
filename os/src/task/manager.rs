6cedcba2b509501dd69e81ef4385b3b19baf6e07
ftq <1310360890@qq.com>
2023-11-03 15:49:23 +0800

    update

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -28,29 +28,6 @@ impl TaskManager {
     }
     /// Take a process out of the ready queue
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
-        // let mut minest_stride: usize=20000000000usize;
-        // let mut new_vec: VecDeque<Arc<TaskControlBlock>>=VecDeque::new();
-        // while !self.ready_queue.is_empty() {
-        //     let now=&self.ready_queue.front().unwrap();
-        //     if now.inner_exclusive_access().task_cx.stride<=minest_stride {
-        //         minest_stride=now.inner_exclusive_access().task_cx.stride;
-        //     }
-        //     new_vec.push_back(self.ready_queue.front().unwrap());
-        // }
-        // let mut flag:bool=false;
-        // let mut ret_task: Option<Arc<TaskControlBlock>>=None;
-        // while !new_vec.is_empty() {
-        //     let now=new_vec.pop_front().unwrap();
-        //     if flag==false&&now.inner_exclusive_access().task_cx.stride==minest_stride {
-        //         flag=true;
-        //         now.inner_exclusive_access().task_cx.stride+=now.inner_exclusive_access().task_cx.pass;
-        //         ret_task=Some(now);
-        //     }else {
-        //         self.ready_queue.push_back(now);
-        //     }
-        // }
-        // ret_task
-
         let mut min_id:usize=0;
         for i in 1..self.ready_queue.len(){
             if self.ready_queue.get(i).unwrap().inner_exclusive_access().task_cx.stride
@@ -58,11 +35,7 @@ impl TaskManager {
                 min_id=i;
             }
         }
-        // let clone_task=min_task.clone();
-        let min_task=self.ready_queue.remove(min_id);
-        // min_task.unwrap().inner_exclusive_access().task_cx.stride+=min_task.unwrap().inner_exclusive_access().task_cx.pass;
-        min_task
-        // self.ready_queue.pop_front()
+        self.ready_queue.remove(min_id)
     }
 }
 
